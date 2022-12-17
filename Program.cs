d3369e6579b80c83f2ce7f7cec0d2a9defb46251
Ilia <ily5100@yandex.ru>
2022-12-18 01:16:20 +0300

    First task

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число");
+string numberFirst = Console.ReadLine() ??"";
+int number1 = int.Parse(numberFirst);
+int result1 = number1;
+
+Console.WriteLine("Введите второе число число");
+string numberSecond = Console.ReadLine();
+int number2 = int.Parse(numberSecond);
+int result2 = number2;
+
+    if (number1 > number2)
+    {
+        Console.WriteLine($"число {number1} больше а число {number2} меньше");
+    }
+
+    Console.WriteLine($"число {number1} меньше а число {number2} больше"); 
